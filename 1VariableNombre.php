0ea2d595624c982c3443cd67150c7167e71cc1f1
SebasHM01 <latiajose24@gmail.com>
2024-05-25 17:23:17 -0700

    Add files via upload

---
+++ 1VariableNombre.php
@@ -0,0 +1,7 @@
+<?php
+// 1.Crear una variable para almacenar el nombre de un estudiante
+$nombreEstudiante = "Juan Pérez";
+
+// Muestra el nombre del estudiante en pantalla
+echo "El nombre del estudiante es: " . $nombreEstudiante;
+?>
